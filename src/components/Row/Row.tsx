eced79c0023f1cd43e90ff608130a7abb2698042
Patric Tysk <patric.tysk@gmail.com>
2022-10-14 08:27:15 +0200

    using blur in row

--- src/components/Row/Row.tsx
+++ src/components/Row/Row.tsx
@@ -8,36 +8,48 @@ type RowProps = {
 }
 
 const Row = forwardRef(({ table, n }: RowProps, ref: ((instance: HTMLInputElement | null) => void) | React.RefObject<HTMLInputElement> | null | undefined) => {
-    const [correct, setCorrect] = useState<boolean | undefined>()
-
+    const [answered, setAnswered] = useState<boolean>(false)
     const [value, setValue] = useState<string>()
+    const [correct, setCorrect] = useState<boolean>()
 
-    const handleKeyDown = (e: React.KeyboardEvent) => {
+    // const handleKeyDown = (e: React.KeyboardEvent) => {
 
-        if (e.code === 'Tab' || e.code === 'Enter') {
-            if (typeof value === 'string' && (Math.round(parseInt(value)) === (table * n))) {
-                setCorrect(true)
-            }
-        }
+    //     if (e.code === 'Tab' || e.code === 'Enter') {
+    //         if (typeof value === 'string' && (Math.round(parseInt(value)) === (table * n))) {
+    //             setCorrect(true)
+    //         }
+    //     }
 
-        if (e.code === 'Enter') {
-            e.preventDefault()
-        }
-    }
+    //     if (e.code === 'Enter') {
+    //         e.preventDefault()
+    //     }
+    // }
 
     return (
         <div
             className={styles.row}
-            onKeyDown={handleKeyDown}
+        //onKeyDown={handleKeyDown}
         >
             <span>{table} * {n}</span>
             <span>=</span>
             <Form className={styles.answer}>
-                <Form.Control ref={ref} type='number' step='1' size='lg' onChange={(e) => {
-                    setValue(e.target.value)
-                }} />
+                <Form.Control
+                    ref={ref}
+                    type='number'
+                    step='1'
+                    size='lg'
+                    onChange={(e) => {
+                        setValue(e.target.value)
+                    }}
+                    onBlur={() => {
+                        setAnswered(true)
+                        if (typeof value === 'string' && (Math.round(parseInt(value)) === (table * n))) {
+                            setCorrect(true)
+                        }
+                    }}
+                />
             </Form>
-            <span>{correct === undefined ? ' ? ' : correct ? ' R ' : 'v'}</span>
+            {answered && <span>{correct ? ' R ' : 'X'}</span>}
         </div>
     )
 })
